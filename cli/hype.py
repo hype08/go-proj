bbe872a2f441741d7b9543ae07521a3d434b4ee2
Henry Zhang <henry.zhang@later.com>
2024-12-04 08:29:19 -0800

    reset db command

--- cli/hype.py
+++ cli/hype.py
@@ -2,6 +2,7 @@
 
 import os
 import typer
+from typing import *
 
 DOCKER_OPTIONS = 'BUILDKIT=1'
 
@@ -10,17 +11,17 @@ app = typer.Typer()
 
 @app.command(help='Builds Hype.')
 def build():
-  docker_execute('build')
+  docker_execute(['build'])
 
 
 @app.command(help='Starts Hype.')
 def start():
-  docker_execute('up')
+  docker_execute(['up --remove-orphans'])
 
 
 @app.command(help='Stops Hype.')
 def stop():
-  docker_execute('down')
+  docker_execute(['down'])
 
 
 @app.command(help='Open a shell into Hype.')
@@ -28,9 +29,24 @@ def shell(app: str):
   docker_execute(f'exec {app} bash')
 
 
-def docker_execute(command: str):
+@app.command(help='Reset the database.')
+def reset_db():
+  docker_execute_sql(['DROP DATABASE IF EXISTS hype WITH (FORCE)'])
+  docker_execute_sql(['CREATE DATABASE hype'])
+
+
+def docker_execute_sql(params: List[str]):
+  '''Executes SQL in the database via a Docker Compose command.'''
+  psql_params = ['host=db', 'user=postgres', 'password=postgres']
+  command = f'run --rm --no-deps db psql "{" ".join(psql_params)}" -c "{" ".join(params)}"'
+  docker_execute([command])
+
+
+def docker_execute(command: List[str]):
   ''' Executes a Docker Compose command. '''
-  os.system(f'{DOCKER_OPTIONS} docker-compose -f {root}/docker-compose.yml {command}')
+  cmd_str = ' '.join(command)
+  os.system(f'{DOCKER_OPTIONS} docker compose -f {root}/docker-compose.yml {cmd_str}')
+
 
 if __name__ == '__main__':
   app()
